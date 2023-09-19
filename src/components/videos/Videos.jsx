30b7833a50074346ded923ef93645e283c21d40f
Tanjil Hossain <tanjilhossain845.com>
2023-09-19 10:36:14 +0600

    compleate vido senary funtionality

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,13 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
 import Video from "./Video";
+import { useEffect } from "react";
+import { fetchVideos } from "../../redux/features/videos/videoSlice";
+import Loading from "../ui/Pagination/Loading";
 
 const Videos = () => {
+  const { videos, isLoading, error, isError } = useSelector(
+    (state) => state.videos
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchVideos());
+  }, []);
+  // desicion making
+  let content = null;
+  if (isLoading) content = <Loading />;
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+  if (!isLoading && !isError && videos?.length == 0)
+    content = <div>No Videos Found !</div>;
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
   return (
     <section className="pt-12">
       <section className="pt-12">
         <div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
           {/* <!-- single video --> */}
-          <Video />
-
+          {content}
           {/* <!-- error section
                     <div className="col-span-12">some error happened</div> --> */}
         </div>
