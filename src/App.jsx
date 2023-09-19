01c97e6dbe0c86be5721f7c351ecca20bef82b81
Tanjil Hossain <tanjilhossain845.com>
2023-09-19 19:27:01 +0600

    moved react router latest version

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/Home";
-import VideoPage from "./pages/VideoPage";
 import Footer from "./components/footer/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos/:videoId" element={<VideoPage />} />
-      </Routes>
+      <Outlet />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,31 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/app/store.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import VideoPage from "./pages/VideoPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/videos/:videoId",
+        element: <VideoPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.Fragment>
 );
