f27f3cb266a1e3c480e03de44817c6c8a3dbcf8a
Tanjil Hossain <tanjilhossain845.com>
2023-09-19 09:20:34 +0600

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
 import VideoPage from "./pages/VideoPage";
 import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      {/* <Home /> */}
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<VideoPage />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
